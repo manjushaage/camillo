e133f24e1af2dd9748a75f36daa446d1fb7675b4
manjushaage <florian.urach@gmail.com>
2024-11-23 14:03:09 +0900

    Upload of first page draft

---
+++ Oliver/index.js
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+
+
+
+
+
+/*{ <script id="wp-block-template-skip-link-js-after">
+        ( function() {
+                var skipLinkTarget = document.querySelector( 'main' ),
+                        sibling,
+                        skipLinkTargetID,
+                        skipLink;
+
+                // Early exit if a skip-link target can't be located.
+                if ( ! skipLinkTarget ) {
+                        return;
+                }
+
+                /*
+                 * Get the site wrapper.
+                 * The skip-link will be injected in the beginning of it.
+                 */
+                /*sibling = document.querySelector( '.wp-site-blocks' );
+
+                // Early exit if the root element was not found.
+                if ( ! sibling ) {
+                        return;
+                }
+
+                // Get the skip-link target's ID, and generate one if it doesn't exist.
+                skipLinkTargetID = skipLinkTarget.id;
+                if ( ! skipLinkTargetID ) {
+                        skipLinkTargetID = 'wp--skip-link--target';
+                        skipLinkTarget.id = skipLinkTargetID;
+                }
+
+                // Create the skip link.
+                skipLink = document.createElement( 'a' );
+                skipLink.classList.add( 'skip-link', 'screen-reader-text' );
+                skipLink.href = '#' + skipLinkTargetID;
+                skipLink.innerHTML = 'Skip to content';
+
+                // Inject the skip link.
+                sibling.parentElement.insertBefore( skipLink, sibling );
+        }() );
+
+
+
+// </script>
+// <script src="https://www.oliver-schmidt.at/wp-includes/js/dist/hooks.js?ver=5b4ec27a7b82f601224a" id="wp-hooks-js"></script>
+// <script src="https://www.oliver-schmidt.at/wp-includes/js/dist/i18n.js?ver=2aff907006e2aa00e26e" id="wp-i18n-js"></script>
+// <script id="wp-i18n-js-after">
+// wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+// </script>
+// <script src="https://www.oliver-schmidt.at/wp-content/plugins/contact-form-7/includes/swv/js/index.js?ver=6.0" id="swv-js"></script>
+// <script id="contact-form-7-js-before">
+// var wpcf7 = {
+//     "api": {
+//         "root": "https:\/\/www.oliver-schmidt.at\/wp-json\/",
+//         "namespace": "contact-form-7\/v1"
+//     },
+//     "cached": 1
+// };
+// </script>
+// <script src="https://www.oliver-schmidt.at/wp-content/plugins/contact-form-7/includes/js/index.js?ver=6.0" id="contact-form-7-js"></script>
+// <script src="https://www.google.com/recaptcha/api.js?render=6LfpwKIoAAAAAFiZiROBfHsglopbsh_RLX1HCDo-&amp;ver=3.0" id="google-recaptcha-js"></script>
+// <script src="https://www.oliver-schmidt.at/wp-includes/js/dist/vendor/wp-polyfill.js?ver=3.15.0" id="wp-polyfill-js"></script>
+// <script id="wpcf7-recaptcha-js-before">
+// var wpcf7_recaptcha = {
+//     "sitekey": "6LfpwKIoAAAAAFiZiROBfHsglopbsh_RLX1HCDo-",
+//     "actions": {
+//         "homepage": "homepage",
+//         "contactform": "contactform"
+//     }
+// };
+// </script>
+// <script src="https://www.oliver-schmidt.at/wp-content/plugins/contact-form-7/modules/recaptcha/index.js?ver=6.0" id="wpcf7-recaptcha-js"></script> */}
