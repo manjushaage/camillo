28b074226bdccc57741606375074498dd0e0ce0e
manjushaage <florian.urach@gmail.com>
2024-11-23 19:57:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camillo RT</title>
+   
+   
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- Google Font - Poppins font input -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">  
+
+    <!-- Boxicon connection -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+
+<header class="header">
+    <a href="#" class="logo">Camillo RT</a>
+
+    <i class='bx bx-menu' id="menu-icon"></i>
+
+    <nav class="navbar">
+        <a href="#home" class="active">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+    </nav>
+</header>
+
+<section class="home" id="home">
+<div class="home-content">
+    <h3>Hello, We are</h3>
+    <h1>Camillo RT</h1>
+    <h3>And we do <span class="multiple-text"></span></h3>
+    <p>Transform ideas into interactive experiences with cutting-edge XR, stunning 3D content,
+        and gamification that captivates your audience.</p>
+        <div class="social-media">
+            <a href="#"><i class='bx bxl-facebook' ></i></a>
+            <a href="#"><i class='bx bxl-twitter' ></i></a>
+            <a href="#"><i class='bx bxl-instagram-alt' ></i></a>
+            <a href="#"><i class='bx bxl-linkedin' ></i></a>
+        </div>
+        <a href="#" class="btn">Welcome !</a>
+</div>
+
+<div class="home-img">
+    <img src="images/3d model.png" alt="3D Model example">
+</div>
+</section>
 
 
+<!-- About ME -->
+<section class="about" id="about">
+    <div class="about-img">
+        <img src="images/Oliver.png" alt="Oliver Schmidt">
+    </div>
+<div class="about-content">
+    <h2 class="heading">About<span>Me</span></h2>
+    <h3>3DX Designer</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Minus odit debitis officia autem eligendi iusto. 
+        Sequi aliquam vel molestias consectetur praesentium 
+        quisquam quasi suscipit atque impedit rerum? Veniam, et eum.</p>
+        <a href="#" class="btn">Read More</a>
+</div>
+</section>
 
 
+<!-- SERVICES -->
 
+<section class="services" id="services">
+    <h2 class="heading">Our <span> Services</span></h2>
 
+<div class="services-container">
+        <div class="services-box">
+        <i class='bx bxs-paint' ></i>
+        <h3> Graphic Design</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quaerat accusantium dolores expedita quibusdam laudantium. Expedita molestias non impedit error aliquam.</p>
+        <a href="#" class="btn">Read More</a>
+    </div>
 
+    <div class="services-box">
+        <i class='bx bx-code-alt' ></i>
+        <h3> 3DX Development</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quaerat accusantium dolores expedita quibusdam laudantium. Expedita molestias non impedit error aliquam.</p>
+        <a href="#" class="btn">Read More</a>
+    </div>
 
+    <div class="services-box">
+        <i class='bx bxs-game' ></i>
+        <h3> Gamification</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quaerat accusantium dolores expedita quibusdam laudantium. Expedita molestias non impedit error aliquam.</p>
+        <a href="#" class="btn">Read More</a>
+    </div>
 
+</div>
+</section>
 
+<section class="portfolio" id="portfolio">
+    <h2 class="heading">Latest <span>Project</span></h2>
 
-/*{ <script id="wp-block-template-skip-link-js-after">
-        ( function() {
-                var skipLinkTarget = document.querySelector( 'main' ),
-                        sibling,
-                        skipLinkTargetID,
-                        skipLink;
+    <div class="portfolio-container">
+        <div class="portfolio-box">
+            <img src="images/portfolio1.jpg" alt="">
+            <div class="portfolio-layer">
+                <h4>Web Design</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Accusantium ea qui nisi eum dolore exercitationem possimus fuga, 
+                    iusto pariatur dolorum.</p>
+                <a href="#"><i class='bx bx-link-external' ></i></a>
+            </div>
+        </div>
+        <div class="portfolio-box">
+            <img src="images/portfolio2.jpg" alt="">
+            <div class="portfolio-layer">
+                <h4>Web Design</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Accusantium ea qui nisi eum dolore exercitationem possimus fuga, 
+                    iusto pariatur dolorum.</p>
+                <a href="#"><i class='bx bx-link-external' ></i></a>
+            </div>
+        </div>
+        <div class="portfolio-box">
+            <img src="images/portfolio3.jpg" alt="">
+            <div class="portfolio-layer">
+                <h4>Web Design</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Accusantium ea qui nisi eum dolore exercitationem possimus fuga, 
+                    iusto pariatur dolorum.</p>
+                <a href="#"><i class='bx bx-link-external' ></i></a>
+            </div>
+        </div>
+        <div class="portfolio-box">
+            <img src="images/portfolio4.jpg" alt="">
+            <div class="portfolio-layer">
+                <h4>Web Design</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Accusantium ea qui nisi eum dolore exercitationem possimus fuga, 
+                    iusto pariatur dolorum.</p>
+                <a href="#"><i class='bx bx-link-external' ></i></a>
+            </div>
+        </div>
+        <div class="portfolio-box">
+            <img src="images/portfolio5.jpg" alt="">
+            <div class="portfolio-layer">
+                <h4>Web Design</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Accusantium ea qui nisi eum dolore exercitationem possimus fuga, 
+                    iusto pariatur dolorum.</p>
+                <a href="#"><i class='bx bx-link-external' ></i></a>
+            </div>
+        </div>
+        <div class="portfolio-box">
+            <img src="images/portfolio6.jpg" alt="">
+            <div class="portfolio-layer">
+                <h4>Web Design</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Accusantium ea qui nisi eum dolore exercitationem possimus fuga, 
+                    iusto pariatur dolorum.</p>
+                <a href="#"><i class='bx bx-link-external' ></i></a>
+            </div>
+        </div>
+    </div>
+</section>
 
-                // Early exit if a skip-link target can't be located.
-                if ( ! skipLinkTarget ) {
-                        return;
-                }
+<section class="contact" id="contact">
+    <h2 class="heading">Contact<span>Me!</span></h2>
+    <form action="#">
+        <div class="input-box">
+            <input type="text" placeholder="Full Name">
+            <input type="email" placeholder="Email Adress">
+        </div>
+        <div class="input-box">
+            <input type="number" placeholder="Mobile Number">
+            <input type="text" placeholder="Email Subject">
+        </div>
+        <textarea namee="" id="" cols="" rows="" placeholder="Your Message"></textarea>
+        <input type="submit" value="Send Message" class="btn">
+    </form>"
+</section>
 
-                /*
-                 * Get the site wrapper.
-                 * The skip-link will be injected in the beginning of it.
-                 */
-                /*sibling = document.querySelector( '.wp-site-blocks' );
+<footer class="footer">
+    <div class="footer-text">
+        <p>Copyright &copy; 2024 by Camillo RT | All rights Reserved.</p>
+    </div>
 
-                // Early exit if the root element was not found.
-                if ( ! sibling ) {
-                        return;
-                }
+<div class="footer-icontop">
+    <a href="#home"><i class='bx bx-up-arrow-alt' ></i></a>
+</div>
 
-                // Get the skip-link target's ID, and generate one if it doesn't exist.
-                skipLinkTargetID = skipLinkTarget.id;
-                if ( ! skipLinkTargetID ) {
-                        skipLinkTargetID = 'wp--skip-link--target';
-                        skipLinkTarget.id = skipLinkTargetID;
-                }
+</footer>
+<!-- Scroll reveal -->
+<script src="https://unpkg.com/scrollreveal"></script>
 
-                // Create the skip link.
-                skipLink = document.createElement( 'a' );
-                skipLink.classList.add( 'skip-link', 'screen-reader-text' );
-                skipLink.href = '#' + skipLinkTargetID;
-                skipLink.innerHTML = 'Skip to content';
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script></script>
 
-                // Inject the skip link.
-                sibling.parentElement.insertBefore( skipLink, sibling );
-        }() );
-
-
-
-// </script>
-// <script src="https://www.oliver-schmidt.at/wp-includes/js/dist/hooks.js?ver=5b4ec27a7b82f601224a" id="wp-hooks-js"></script>
-// <script src="https://www.oliver-schmidt.at/wp-includes/js/dist/i18n.js?ver=2aff907006e2aa00e26e" id="wp-i18n-js"></script>
-// <script id="wp-i18n-js-after">
-// wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
-// </script>
-// <script src="https://www.oliver-schmidt.at/wp-content/plugins/contact-form-7/includes/swv/js/index.js?ver=6.0" id="swv-js"></script>
-// <script id="contact-form-7-js-before">
-// var wpcf7 = {
-//     "api": {
-//         "root": "https:\/\/www.oliver-schmidt.at\/wp-json\/",
-//         "namespace": "contact-form-7\/v1"
-//     },
-//     "cached": 1
-// };
-// </script>
-// <script src="https://www.oliver-schmidt.at/wp-content/plugins/contact-form-7/includes/js/index.js?ver=6.0" id="contact-form-7-js"></script>
-// <script src="https://www.google.com/recaptcha/api.js?render=6LfpwKIoAAAAAFiZiROBfHsglopbsh_RLX1HCDo-&amp;ver=3.0" id="google-recaptcha-js"></script>
-// <script src="https://www.oliver-schmidt.at/wp-includes/js/dist/vendor/wp-polyfill.js?ver=3.15.0" id="wp-polyfill-js"></script>
-// <script id="wpcf7-recaptcha-js-before">
-// var wpcf7_recaptcha = {
-//     "sitekey": "6LfpwKIoAAAAAFiZiROBfHsglopbsh_RLX1HCDo-",
-//     "actions": {
-//         "homepage": "homepage",
-//         "contactform": "contactform"
-//     }
-// };
-// </script>
-// <script src="https://www.oliver-schmidt.at/wp-content/plugins/contact-form-7/modules/recaptcha/index.js?ver=6.0" id="wpcf7-recaptcha-js"></script> */}
+<script src="js/script.js"></script>
+</body>
+</html>
